5912f81f0167a93aa6a4e2453690af3f88a580cb
pooja-shivlab <pooja.ahuja@shivlab.com>
2024-07-05 12:21:48 +0530

    Corrected component logic and 3 decimal digit allowed

--- odoo/addons/custom_product_component/models/product_component.py
+++ odoo/addons/custom_product_component/models/product_component.py
@@ -22,14 +22,14 @@ class ChemicalComponent(models.Model):
     _name = 'chemical.component'
     _description = 'Chemical Component'
 
-    product_id = fields.Many2one('product.template', string='Product', ondelete='cascade')
-    component_id = fields.Many2one('product.component', string='Component')
-    component_name = fields.Char(string='Component Name', related='component_id.name', store=True)
-    value_type = fields.Selection([
-        ('value_range', 'Value Range'),
-        ('fix_value', 'Fix Value')],
-        string='Component Value', required=True, default='value_range')
-    min_value = fields.Float(string='Min Value (in %)')
-    max_value = fields.Float(string='Max/Fixed Value (in %)')
-    price = fields.Float(string='Price')
+    name = fields.Char(string='Name', required=True)
+    short_name = fields.Char(string='Short Name')
     is_default = fields.Boolean(default=False, string='Default')
+
+    def toggle_default(self, *args):
+        for record in self:
+            record.is_default = True
+
+    def remove_toggle_default(self, *args):
+        for record in self:
+            record.is_default = False
--- odoo/addons/custom_product_component/models/product_template.py
+++ odoo/addons/custom_product_component/models/product_template.py
@@ -1,8 +1,39 @@
 from odoo import models, fields, api
 
 
+class ProductTemplateComponentRel(models.Model):
+    _name = 'product.template.component.rel'
+    _description = 'Product Template Component Relation'
+
+    product_template_id = fields.Many2one('product.template', string='Product Template', required=True)
+    product_component_id = fields.Many2one('product.component', string='Product Component', required=True)
+    value_type = fields.Selection([
+        ('value_range', 'Value Range'),
+        ('fix_value', 'Fix Value')],
+        string='Component Value', required=True, default='value_range')
+    min_value = fields.Float(string='Min Value (in %)', digits=(12, 3))
+    max_value = fields.Float(string='Max / Fixed Value (in %)', digits=(12, 3))
+
+
+class ProductTemplateChemicalComponentRel(models.Model):
+    _name = 'product.template.chemical.component.rel'
+    _description = 'Product Template Chemical Component Relation'
+
+    template_id = fields.Many2one('product.template', string='Product Template', required=True)
+    product_component_id = fields.Many2one('chemical.component', string='Chemical Component', required=True)
+    value_type = fields.Selection([
+        ('value_range', 'Value Range'),
+        ('fix_value', 'Fix Value')],
+        string='Component Value', required=True, default='value_range')
+    min_value = fields.Float(string='Min Value (in %)', digits=(12, 3))
+    max_value = fields.Float(string='Max / Fixed Value (in %)', digits=(12, 3))
+    price = fields.Float(string='Price')
+
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    chemical_component_ids = fields.One2many('chemical.component', 'product_id', string='Chemical Components')
-    pricing_ids = fields.One2many('chemical.component', 'product_id', string='Pricing')
+    component_ids = fields.One2many('product.template.component.rel', 'product_template_id',
+                                    string='Product Components')
+    chemical_component_ids = fields.One2many('product.template.chemical.component.rel', 'template_id',
+                                             string='Chemical Components')
